2e65cf495bb1c36f227d37edd4fd52e10dd65501
A Roy <agni_18_roy@yahoo.com>
2017-07-12 23:25:38 +0530

    read/readOne ok

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -7,5 +7,27 @@ module.exports = {
             .create(item_info)
             .then((item) => res.send(item))
             .catch(next);
-    }
+    },
+
+    read: function (req, res, next) {
+        Item
+            .find({})
+            .then((items) => {
+                console.log(items);
+                res.send(items)
+            })
+            .catch(next);
+    },
+
+    readOne: function (req, res, next) {
+        Item
+            .find({
+                _id: req.params.id
+            })
+            .then((items) => {
+                console.log(items);
+                res.send(items)
+            })
+            .catch(next);
+    },
 }
