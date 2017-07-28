9b7dabc4c4ac6786e0c4e60695b70f6fe1c28d0a
Agnimitra Roy <agnimitraroy@Agnimitras-MacBook-Pro.local>
2017-07-28 11:34:11 +0530

    somewhat cooler navbar

--- views/components/nav_bar.js
+++ views/components/nav_bar.js
@@ -1,17 +1,34 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
-function nav() {
-  return (
-    <ul className='nav'>
-      <li className="col-xs-2">
-        <Link to='/items/new'>ADD</Link>
-      </li>
-      <li className="col-xs-2">
-        <Link to='/items'>HOME</Link>
-      </li>
-    </ul>
-  );
+class nav extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: ''
+    }
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-inverse">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="/">SUV</a>
+          </div>
+          <ul className="nav navbar-nav">
+            <li>
+              <Link to='/items/new'>NEW</Link>
+            </li>
+            <li>
+              <Link to='/items'>ALL</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
 module.exports = nav;
